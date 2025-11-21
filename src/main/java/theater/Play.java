3224dd3c19ae836f06cc726de1f41acb89301960
lyzsa <susanli0902@outlook.com>
2025-11-21 18:34:49 -0500

    Task 2.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,37 @@
 package theater;
 
+/**
+ * Represents a theatrical play with a name and a type (e.g., Drama, Comedy).
+  */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
+
+    /**
+     * Constructs a new Play object.
+     *
+     * @param name The title of the play. **(non-null)**
+     * @param type The genre of the play. **(non-null)**
+     * @throws IllegalArgumentException if name or type are null or empty.
+     */
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    /**
+     * Gets the name of the play.
+     *
+     * @return The name of the play.
+     */
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
